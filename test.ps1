dcd02c816ebad34408ec45871779e15a99ef23b0
Paul Kostic <paulkostic@gmail.com>
2026-06-13 18:51:45 -0700

    class based changes

--- test.ps1
+++ test.ps1
@@ -1,114 +1,160 @@
-# Connecting to IOLAN via TCP===============================================================================================
-
-
+#===============================================================================
+# Program:  test.ps1 (IOLAN-Bridge testing script)
+# Author:   Paul Kostic
+# Date:     6/13/2026
+#===============================================================================
+# This program assumes that the IOLAN has been configured per the README
+#===============================================================================
 
 # $Inteface_list = [System.Net.NetworkInformation.NetworkInterface]::GetAllNetworkInterfaces()
 
-$IOLAN_IP = "fe80::280:d4ff:fe32:80a8" # Hardcoded for now (next-step: dynamic checking of single valid IP on each interface list)
-$Ethernet_Interface = "enp0s31f6"      # Hardcoded for now (next-step: dynamic testing and list with interface_list)
+class test {
 
-$ADDR = $IOLAN_IP + "%"
-$ADDR = $ADDR + $Ethernet_Interface
+    # $IPV6 = "fe80::280:d4ff:fe32:80a8"
+    # $Eth = "enp0s31f6"
 
-$addr = [System.Net.IPAddress]::Parse("fe80::280:d4ff:fe32:80a8%enp0s31f6")
+    # Serial Port Object
+    [System.IO.Ports.SerialPort]$DB9
 
+    # TCP Socket Object
+    [System.Net.Sockets.Socket]$TCP
 
-$tcp_config = New-Object System.Net.IPEndPoint($addr,10010)
+    # TCP EndPoint Object
+    [System.Net.IPEndPoint]$TCPEND
 
-$tcp = New-Object System.Net.Sockets.Socket(
-    [System.Net.Sockets.AddressFamily]::InterNetworkV6,
-    [System.Net.Sockets.SocketType]::Stream,
-    [System.Net.Sockets.ProtocolType]::Tcp
-)
+    #buffer
+    $buffer
 
-while (1){
-    try{
-        Write-Host "Connecting TCP->IOLAN: " -NoNewline
-        $tcp.Connect($tcp_config) # open the serial port
-        Write-Host "Connected" -ForegroundColor Green
-    } catch {
-        Write-Host ("FAILED") -ForegroundColor DarkYellow
-        Write-Host "Exception: $($_.Exception.Message)" -ForegroundColor Red
-        $exit = Read-Host "Enter exit or enter to try again"
+    # Constructor ==============================================================
+    test() {
+        # Set TCP Socket Data
+        $this.TCP = [System.Net.Sockets.Socket]::new(
+            [System.Net.Sockets.AddressFamily]::InterNetworkV6,
+            [System.Net.Sockets.SocketType]::Stream,
+            [System.Net.Sockets.ProtocolType]::Tcp
+        )
 
-        if ($exit -eq "exit"){
-            return;
-        }else{
-            continue;
+        # Set TCP Endpoint Data
+        # Parse IPv6 WITHOUT interface name
+        $ip = [System.Net.IPAddress]::Parse("fe80::280:d4ff:fe32:80a8")
+
+        $this.TCPEND = [System.Net.IPEndPoint]::new(
+            $ip,                # 
+            10010               # port used
+        )
+
+        # Set Serial Port Data
+        $this.DB9               = New-Object System.IO.Ports.SerialPort
+        $this.DB9.PortName      = "/dev/ttyUSB0"
+        $this.DB9.BaudRate      = 19200
+        $this.DB9.Parity        = [System.IO.Ports.Parity]::Even
+        $this.DB9.DataBits      = 7
+        $this.DB9.StopBits      = [System.IO.Ports.StopBits]::One
+        $this.DB9.Handshake     = [System.IO.Ports.Handshake]::None
+        $this.DB9.ReadTimeout   = 200
+        $this.DB9.WriteTimeout  = 200
+
+        #create buffers
+        $this.buffer = New-Object byte[] 40961
+    }
+
+    [void]des() {
+        $this.TCP.Close()
+        $this.DB9.Close()
+    }
+
+    [void]TCP_connect() {
+
+        while (1){
+            try{
+                Write-Host "Connecting TCP->IOLAN: " -NoNewline
+                $this.TCP.Connect($this.TCPEND) # open the serial port
+                Write-Host "Connected" -ForegroundColor Green
+            }catch{
+                Write-Host ("FAILED") -ForegroundColor DarkYellow
+                Write-Host "Exception: $($_.Exception.Message)" -ForegroundColor Red
+                $exit = Read-Host "Enter exit or enter to try again"
+
+                if ($exit -eq "exit"){
+                    return;
+                }else{
+                    continue;
+                }
+            } break;
         }
     }
-    break;
-}
 
-# Connecting to IOLAN via DB9===============================================================================================
-$DB9                 = New-Object System.IO.Ports.SerialPort
-$DB9.PortName        = "/dev/ttyUSB0"
-$DB9.BaudRate        = 19200
-$DB9.Parity          = [System.IO.Ports.Parity]::Even
-$DB9.DataBits        = 7
-$DB9.StopBits        = [System.IO.Ports.StopBits]::One
-$DB9.Handshake       = [System.IO.Ports.Handshake]::None
-$DB9.ReadTimeout     = 200
-$DB9.WriteTimeout    = 200
+    # Connecting to IOLAN via DB9===============================================
 
-$validPorts = [System.IO.Ports.SerialPort]::GetPortNames()
+    [void]DB9_connect() {
 
-Write-Host "Valid Port Names: $validPorts"
+        $validPorts = [System.IO.Ports.SerialPort]::GetPortNames()
 
-Write-Host "Connecting SIM->IOLAN: " -NoNewline
-try{
-    $DB9.Open()
-    Write-Host "Connected`n" -ForegroundColor Green
-}catch{
-    Write-Host ("FAILED") -ForegroundColor DarkYellow
-    Write-Host "Exception: $($_.Exception.Message)" -ForegroundColor Red
-}
+        Write-Host "Valid Port Names: $validPorts"
 
-
-$buffer = New-Object byte[] 40961
-
-# Main Loop ================================================================================================================
-
-while ($true) {
-    $tx = Read-Host "TCP TX"
-
-    if ($tx -eq "exit") {
-        break
-    }
-
-    # Send ASCII text
-    $bytes = [System.Text.Encoding]::ASCII.GetBytes($tx)
-    [void]$tcp.Send($bytes)
-
-    # wait 200 ms before receiving message
-    Start-Sleep -Milliseconds 200
-
-    # receive data via serial
-    $raw_DB9_rx = $DB9.ReadExisting()
-
-    Write-Host ("SIM RX: :`"$DB9_rx`"")
-    
-    # send serial send example serial reDB9onse
-    $example = ":01040200CE2B"
-
-    $DB9.Write($example + "`r`n")
-    Write-Host("SIM TX: `"$example`"")
-
-    # Receive reDB9onse via tcp
-    if ($tcp.Poll(1000000, [System.Net.Sockets.SelectMode]::SelectRead)) {
-        $count = $tcp.Receive($buffer)
-
-        if ($count -gt 0) {
-            $raw_tcp_rx = [System.Text.Encoding]::ASCII.GetString($buffer,0,$count)
-
-            Write-Host "TCP RX: `":$raw_tcp_rx`" `n"
+        Write-Host "Connecting SIM->IOLAN: " -NoNewline
+        try{
+            $this.DB9.Open()
+            Write-Host "Connected`n" -ForegroundColor Green
+        }catch{
+            Write-Host ("FAILED") -ForegroundColor DarkYellow
+            Write-Host "Exception: $($_.Exception.Message)" -ForegroundColor Red
         }
-    } else {
-        Write-Host "(No reDB9onse) `n"
     }
-}
 
-$tcp.Close()
-$DB9.Close()
+    # Main Loop ================================================================
+    [void]main() {
 
-# END ======================================================================================================================
+        $this.TCP_connect()
+
+        $this.DB9_connect()
+
+        while ($true) {
+            $tx = Read-Host "TCP TX"
+
+            if ($tx -eq "exit") {
+                break}
+
+            # Send ASCII text
+            $bytes = [System.Text.Encoding]::ASCII.GetBytes($tx)
+            [void]$this.TCP.Send($bytes)
+
+            # wait 50 ms before receiving message
+            Start-Sleep -Milliseconds 50
+
+            # receive data via serial
+            $raw_DB9_rx = $this.DB9.ReadExisting()
+
+            Write-Host ("SIM RX: :`"$raw_DB9_rx`"")
+
+            # send serial send example serial reDB9onse
+            $example = ":01040200CE2B"
+
+            $this.DB9.Write($example + "`r`n")
+            Write-Host("SIM TX: `"$example`"")
+
+            # Receive response via tcp
+            if ($this.TCP.Poll(1000000, [System.Net.Sockets.SelectMode]::SelectRead)) {
+                $count = $this.TCP.Receive($this.buffer)
+
+                if ($count -gt 0) {
+                    $raw_tcp_rx = [System.Text.Encoding]::ASCII.GetString($this.buffer,0,$count)
+
+                    Write-Host "TCP RX: `":$raw_tcp_rx`" `n"
+                }
+            } else {
+                Write-Host "(No response) `n"
+            }
+        } # end Loop
+
+    } # END MAIN
+
+} # END  TEST ==================================================================
+
+# Main things ==================================================================
+
+$test = [test]::new()
+$test.main()
+$test.des()
+
+# End ==========================================================================
